0378c23823b3877bfbfa43f35a0ac37e3d1ea6ea
Umlen <victorpatrin@gmail.com>
2023-12-10 12:19:36 +0700

    issues fixed

--- src/components/GridView/GridDays.tsx
+++ src/components/GridView/GridDays.tsx
@@ -1,41 +1,46 @@
 import { useState, type FunctionComponent, useEffect } from 'react';
 import testData from '../../data/testCalendar.json';
-import { checkGridDaysFlag, getDaysFromCalendar } from '../../utils/utils';
+import { checkCalendarSeparator, getDaysFromCalendar } from '../../utils/utils';
 import SquareButton from '../ui/Buttons/SquareButton';
 import DayInGrid from './DayInGrid';
 import styles from './gridDays.module.scss';
 
 const GridDays: FunctionComponent = () => {
+  const AMOUNT_OF_DISPLAYED_DAYS = 15;
   const [isStart, setIsStart] = useState(false);
   const [isEnd, setIsEnd] = useState(true);
-  const [flag, setFlag] = useState(15);
-  const [days, setDays] = useState(getDaysFromCalendar(testData.days, flag));
+  const [calendarSeparator, setCalendarSeparator] = useState(
+    AMOUNT_OF_DISPLAYED_DAYS,
+  );
+  const [days, setDays] = useState(
+    getDaysFromCalendar(testData.days, calendarSeparator),
+  );
 
   useEffect(() => {
-    const { checkedIsStart, checkedIsEnd } = checkGridDaysFlag(
-      flag,
+    const { checkedIsStart, checkedIsEnd } = checkCalendarSeparator(
+      calendarSeparator,
       testData.days.length,
     );
 
     setIsStart(checkedIsStart);
     setIsEnd(checkedIsEnd);
-  }, [flag]);
+  }, [calendarSeparator]);
 
-  function increaseFlag(): void {
-    const newFlag = flag + 15;
-    setFlag(newFlag);
-    setDays(getDaysFromCalendar(testData.days, newFlag));
+  function increaseCalendarSeparator(): void {
+    const newCalendarSeparator = calendarSeparator + AMOUNT_OF_DISPLAYED_DAYS;
+    setCalendarSeparator(newCalendarSeparator);
+    setDays(getDaysFromCalendar(testData.days, newCalendarSeparator));
   }
 
-  function decreaseFlag(): void {
-    const newFlag = flag - 15;
-    setFlag(newFlag);
-    setDays(getDaysFromCalendar(testData.days, newFlag));
+  function decreaseCalendarSeparator(): void {
+    const newCalendarSeparator = calendarSeparator - AMOUNT_OF_DISPLAYED_DAYS;
+    setCalendarSeparator(newCalendarSeparator);
+    setDays(getDaysFromCalendar(testData.days, newCalendarSeparator));
   }
 
   return (
     <div className="flexRow">
-      <SquareButton disabled={isStart} onClick={increaseFlag}>
+      <SquareButton disabled={isStart} onClick={increaseCalendarSeparator}>
         <p>&lt;</p>
       </SquareButton>
       <div className={styles.daysWrapper}>
@@ -43,7 +48,7 @@ const GridDays: FunctionComponent = () => {
           <DayInGrid key={day._id} id={day._id} date={day.date} />
         ))}
       </div>
-      <SquareButton disabled={isEnd} onClick={decreaseFlag}>
+      <SquareButton disabled={isEnd} onClick={decreaseCalendarSeparator}>
         <p>&gt;</p>
       </SquareButton>
     </div>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,28 +1,6 @@
 import { type CalendarType } from '../types/types';
 
-export const getCurrentDate = (): string => {
-  const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = String(date.getFullYear()).slice(-2);
-
-  return [day, month, year].join(' / ');
-};
-
-export const getDaysFromCalendar = (
-  calendar: CalendarType[],
-  flag: number,
-): CalendarType[] => {
-  const spliceStart = calendar.length - flag < 0 ? 0 : calendar.length - flag;
-  return [...calendar].splice(spliceStart, 15);
-};
-
-export const getDayById = (
-  calendar: CalendarType[],
-  id: string,
-): CalendarType => {
-  return calendar.filter((day) => day._id === id)[0];
-};
+const AMOUNT_OF_DISPLAYED_DAYS = 15;
 
 export const getLocalStorageTheme = (): string => {
   const savedTheme = localStorage.getItem('SuccessDiaryTheme');
@@ -34,21 +12,49 @@ export const getLocalStorageTheme = (): string => {
   return 'light';
 };
 
-export const checkGridDaysFlag = (
-  flag: number,
-  dataLength: number,
+export const getCurrentDate = (): string => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = String(date.getFullYear()).slice(-2);
+
+  return [day, month, year].join(' / ');
+};
+
+export const getDaysFromCalendar = (
+  calendar: CalendarType[],
+  calendarSeparator: number,
+): CalendarType[] => {
+  const sliceStart =
+    calendar.length - calendarSeparator < 0
+      ? 0
+      : calendar.length - calendarSeparator;
+  const sliceEnd = sliceStart + AMOUNT_OF_DISPLAYED_DAYS;
+  return calendar.slice(sliceStart, sliceEnd);
+};
+
+export const checkCalendarSeparator = (
+  calendarSeparator: number,
+  calendarLength: number,
 ): { checkedIsStart: boolean; checkedIsEnd: boolean } => {
-  if (dataLength === 15) {
+  if (calendarLength === AMOUNT_OF_DISPLAYED_DAYS) {
     return { checkedIsStart: true, checkedIsEnd: true };
   }
 
-  if (flag >= dataLength) {
+  if (calendarSeparator >= calendarLength) {
     return { checkedIsStart: true, checkedIsEnd: false };
   }
 
-  if (flag === 15) {
+  if (calendarSeparator === AMOUNT_OF_DISPLAYED_DAYS) {
     return { checkedIsStart: false, checkedIsEnd: true };
   }
 
   return { checkedIsStart: false, checkedIsEnd: false };
+};
+
+export const getDayById = (
+  calendar: CalendarType[],
+  id: string,
+): CalendarType => {
+  return calendar.filter((day) => day._id === id)[0];
 };
